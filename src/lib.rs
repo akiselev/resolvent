708dd0a67e27d296ca1024e7b531278bb756b131
Alexander Kiselev <github@akiselev.com>
2026-08-16 06:52:57 -0700

    export mixed/vector reference families

--- src/lib.rs
+++ src/lib.rs
@@ -27,6 +27,7 @@ pub mod model;
 pub mod operator;
 pub mod physics;
 pub mod reference;
+pub mod reference_mixed;
 pub mod refinement;
 pub mod rsl;
 pub mod source;
@@ -89,6 +90,10 @@ pub use physics::{
 pub use reference::{
     BoundaryEdge, DofLayout, PiecewiseConstant, ReferenceError, ReferenceMesh2, ReferenceOperator,
     RobinData, ScalarH1Problem, SparseMatrix, compile_scalar_h1_p1,
+};
+pub use reference_mixed::{
+    ElasticityOperator2, IsotropicElasticity2, NedelecOperator2, StokesOperatorP1P1,
+    compile_elasticity_p1_2d, compile_nedelec0_2d, compile_stokes_p1p1_2d,
 };
 pub use refinement::{
     ArtifactKind, ArtifactRef, RefinementError, RefinementProvenance, RefinementRecord,
